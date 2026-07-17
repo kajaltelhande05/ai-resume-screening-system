dee6616f91a08d6d26a6b5a1ee7e16d176a46580
kajaltelhande05 <kajaltelhande642@gmail.com>
2026-07-17 16:58:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import streamlit as st
+import random
 
 st.title("AI Resume Screening System")
 
 uploaded_file = st.file_uploader("Upload Resume", type=["pdf", "docx"])
 
 if uploaded_file is not None:
+    score = random.randint(50, 100)
+
     st.success(f"Uploaded: {uploaded_file.name}")
-    st.write("Resume Score: 85%")
+    st.write(f"Resume Score: {score}%")
+
+    if score >= 85:
+        st.success("✅ Good Resume")
+    elif score >= 70:
+        st.warning("🟡 Average Resume")
+    else:
+        st.error("❌ Poor Resume")
+
+else:
+    st.warning("Please upload your resume.")
